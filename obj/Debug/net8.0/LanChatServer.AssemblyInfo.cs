b22cc5b0c124fbe7c39b4b437579bae31f66807a
demigreeen <158215601+demigreeen@users.noreply.github.com>
2026-05-10 12:08:56 +0300

    add railway config

--- obj/Debug/net8.0/LanChatServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/LanChatServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092435875076f432295dc4bdb82284e54b733fd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c225acc9f6dd7b61fd8be45e71f7bd76ed6e8acb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
